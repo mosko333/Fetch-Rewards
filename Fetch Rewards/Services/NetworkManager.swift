fc92eea37be906f59f593d110e6391f099f5ae15
AdamAM <adamam@gmail.com>
2020-10-20 16:14:58 -0600

    NetworkManager: Adds fetchListItems method

--- Fetch Rewards/Services/NetworkManager.swift
+++ Fetch Rewards/Services/NetworkManager.swift
@@ -9,4 +9,30 @@ import Foundation
 
 class NetworkManager {
     static let shared = NetworkManager()
+
+    func fetchListItems(completed: @escaping (Result<[ListItem], APIServiceError>) -> Void) {
+        guard let url = Endpoint.url else { completed(.failure(.invalidUrl))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { result in
+            switch result {
+            case .success(let (data, response)):
+                guard let statusCode = (response as? HTTPURLResponse)?.statusCode, 200..<300 ~= statusCode else {
+                    completed(.failure(.invalidResponse))
+                    return
+                }
+                do {
+                    let listItems = try JSONDecoder().decode([ListItem].self, from: data)
+                    completed(.success(listItems))
+                } catch {
+                    completed(.failure(.decodeError))
+                    return
+                }
+            case .failure(let error):
+                print(error)
+                completed(.failure(.apiError))
+            }
+        }.resume()
+    }
 }
