83f4b9d861b285b52fa092928def30ee5c518aba
AdamAM <adamam@gmail.com>
2020-10-20 12:27:04 -0600

    Organize file structure

--- Fetch Rewards/Fetch_RewardsApp.swift
+++ Fetch Rewards/Fetch_RewardsApp.swift
@@ -15,3 +15,9 @@ struct Fetch_RewardsApp: App {
         }
     }
 }
+
+struct Fetch_RewardsApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
