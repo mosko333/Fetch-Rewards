ba83b5861aa844c8dacba7e2cbdd44289dde4b26
AdamAM <adamam@gmail.com>
2020-10-20 11:39:27 -0600

    Initial Commit

---
+++ Fetch Rewards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fetch Rewards
+//
+//  Created by Adam on 20/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fetch Rewards/Fetch_RewardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RewardsApp.swift
+//  Fetch Rewards
+//
+//  Created by Adam on 20/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RewardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
